3a74005ba8b2b37cbf0af1ea04aa0f4fe0c4f348
Melo905 <70447904+Melo905@users.noreply.github.com>
2020-11-18 16:18:14 -0300

    Update bcc.py

--- bcc.py
+++ bcc.py
@@ -6,8 +6,27 @@ from datetime_test import calcula_dias
 def create_column_base(assunto,time_min,start_day,end_day):
     # Chamar função para criar as datas para a tabela:
     df1, topic_rows = calcula_dias(start_day, end_day)
-    print(df1)
-    print(topic_rows)
+    #criando coluna do assunto com os tempos em minutos:
+    df2 = pd.DataFrame({assunto:["%.2f"%(time_min)+"min"]}) 
+    x = 0
+    indice = 1 #progressão das linhas preenchidas
+    for indice in range(1,topic_rows+1):
+        #Fórmula (provisória):
+        time_min = time_min * 0.7
+        if time_min > 1:
+            df2.at[indice, assunto] = "%.2f"%(time_min)+" min" #inserção do tempo calculado na coluna e linha respectiva
+        if time_min <=1:
+            df2.at[indice, assunto] = "1.00 min"
+        indice = indice + 1
+    frames = [df1,df2]
+    df = pd.concat(frames, axis = 1, ignore_index=False)
+    df.rename(columns={"0": "Data", "1": assunto})
+    return df
+
+
+def create_and_app(df_current,assunto,time_min,start_day,end_day):
+    # Chamar função para criar as datas para a tabela:
+    df1, topic_rows = calcula_dias(start_day, end_day)
     #criando coluna do assunto com os tempos em minutos:
     df2 = pd.DataFrame({assunto:["%.2f"%(time_min)+"min"]}) 
     x = 0
@@ -17,17 +36,15 @@ def create_column_base(assunto,time_min,start_day,end_day):
         time_min = time_min * 0.7
         df2.at[indice, assunto] = "%.2f"%(time_min)+"min" #inserção do tempo calculado na coluna e linha respectiva
         indice = indice + 1
-    print(df2)
     frames = [df1,df2]
     df = pd.concat(frames, axis = 1, ignore_index=False)
     df.rename(columns={"0": "Data", "1": assunto})
-    df.to_csv('Multivac.csv', index=False)
-
-
+    df = pd.concat([df_current,df], axis = 1, ignore_index=False)
+    return df
     
 
 def new_file():#solicita info essenciais para criar uma coluna na tabela
-    flag = 1 # contar os atributos colocados(limite de 10)
+     # flag -> contar os atributos colocados(limite de 10)
     for flag in range(1,10):
         # Info fornecida pelo usuário
         assunto = input("Assunto %d:" % flag)
@@ -37,20 +54,22 @@ def new_file():#solicita info essenciais para criar uma coluna na tabela
 
         # Primeiro assunto cria a tabela (os posteriores são emendados à primeira)
         if flag == 1:
-            create_column_base(assunto,time_min,start_day,end_day)
+            retorno = create_column_base(assunto,time_min,start_day,end_day)
             
         # Para mais de um assunto
-        if flag > 1:
-            create_and_app()#"Create and append"
+        if flag > 1 and flag <=10:
+            retorno = create_and_app(retorno,assunto,time_min,start_day,end_day)#"Create and append"
 
-        flag = flag + 1
-
-        # Pergunta se vai continuar a iteração
-        #mais = input("Adicionar outro assunto? S ou N")
-        #if mais == "S" or mais == "s":
-           # x = 0
-       # if mais == "N" or mais == "n":
-            #x = 2
+        if flag == 10:
+            retorno.to_csv('Multivac.csv', index=False)
+        #Pergunta se vai continuar a iteração
+        mais = input("Adicionar outro assunto? S ou N\n")
+        if mais == "S" or mais == "s":
+            flag = flag + 1
+        if mais == "N" or mais == "n":
+            flag = 11
+            retorno.to_csv('Multivac.csv', index=False)
+            print("Seu arquivo está pronto. Olhe na pasta do programa!")
 
     # Ao final das iterações, salvar a tabela        
     print(df)
@@ -73,11 +92,6 @@ if choice1 == "2":
     mod_file()
 if choice1 == "3":
     print("Programa terminado.")
-
-    
-
-
-
 
     
 
