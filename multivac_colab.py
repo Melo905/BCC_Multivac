b30e1090f82d5672dcb9978511f4f01e64aeb5e8
Melo905 <70447904+Melo905@users.noreply.github.com>
2020-11-28 16:12:02 -0300

    Update multivac_colab.py

--- multivac_colab.py
+++ multivac_colab.py
@@ -5,77 +5,85 @@ import pandas as pd
 
 #Funções que operam datas
 def calcula_dias(date1,validade):
-    date1 = date.fromisoformat(date1)
-    flag = 1
-    indice = 1
-    df = pd.DataFrame({"Dia":[date1]})
-#A partir daí, calcular a diferença de dias em que o usuário vai estudar
-#e só imprimir as linhas com esses dias no dataframe.
-#flag = n° de dias depois do primeiro estudo
-    if flag == 1:
-        date2 = date1 + timedelta(days = 1)#24h depois do estudo
-        df.at[indice,"Dia"] = str(date2)
-        flag = 7
-        topic_rows = 1#Quantas linhas são de fato, para que a coluna de assunto bata
-        indice = indice + 1
-    if flag == 7: # condição é a soma dos dias totais passados desde a primeira sessão
-        date3 = date1 + timedelta(days = 7)#1 semana depois do estudo
-        df.at[indice,"Dia"] = str(date3)
-        flag = 30
-        indice = indice + 1
-        topic_rows = topic_rows + 1
-    if flag == 30:
-        date4 = date1 + timedelta(days = 30)#1 mês depois do estudo
-        df.at[indice,"Dia"] = str(date4)
-        indice = indice + 1
-        flag = 60
-        topic_rows = topic_rows + 1
-    for flag in range(60,int(validade),30):
-        if flag % 30 == 0:
-            date4 = date4 + timedelta(days =30)#intervalos de dias constantes (30 em 30 dias)
-            df.at[indice,"Dia"] = str(date4)
-            indice = indice + 1
-            topic_rows = topic_rows + 1        
-    df1 = df
-    return df1,topic_rows
+   while True:
+    try:
+      date1 = date.fromisoformat(date1)
+      flag = 1
+      indice = 1
+      df = pd.DataFrame({"Dia":[date1]})
+      if flag == 1: #flag = n° de dias depois do primeiro estudo
+          date2 = date1 + timedelta(days = 1)#24h depois do estudo
+          df.at[indice,"Dia"] = str(date2)
+          flag = 7
+          topic_rows = 1#Quantas linhas são de fato, para que a coluna de assunto bata
+          indice = indice + 1
+      if flag == 7: # condição é a soma dos dias totais passados desde a primeira sessão
+          date3 = date1 + timedelta(days = 7)#1 semana depois do estudo
+          df.at[indice,"Dia"] = str(date3)
+          flag = 30
+          indice = indice + 1
+          topic_rows = topic_rows + 1
+      if flag == 30:
+          date4 = date1 + timedelta(days = 30)#1 mês depois do estudo
+          df.at[indice,"Dia"] = str(date4)
+          indice = indice + 1
+          flag = 60
+          topic_rows = topic_rows + 1
+      for flag in range(60,int(validade),30):
+          if flag % 30 == 0:
+              date4 = date4 + timedelta(days =30)#intervalos de dias constantes (30 em 30 dias)
+              df.at[indice,"Dia"] = str(date4)
+              indice = indice + 1
+              topic_rows = topic_rows + 1 
+      df1 = df
+      return df1,topic_rows    
+      break
+    except (ValueError,TypeError):
+      print("Data apenas aceita no formato 'YYYY-MM-DD'!\n")
+      date1 = input("Insira a data de início de contagem dessa forma '2020-12-30':")#função de data
+      validade = int(input("Prazo de validade (em dias):"))#função de data   
 
-#mudar o número do atributo "Dias.n"
-#usando 'coluna'
-
-def calcula_dias_dif(coluna,date1,validade):
-    date1 = date.fromisoformat(date1)
-    flag = 1
-    indice = 1
-    data = "Dia " + str(coluna)
-    df = pd.DataFrame({data:[date1]})
-    if flag == 1:
-        date2 = date1 + timedelta(days = 1)
-        df.at[indice,data] = date2
-        flag = 7
-        topic_rows = 1 
-        indice = indice + 1
-    if flag == 7: 
-        date3 = date1 + timedelta(days = 7)
-        df.at[indice,data] = date3
-        flag = 30
-        indice = indice + 1
-        topic_rows = topic_rows + 1
-    if flag == 30:
-        date4 = date1 + timedelta(days = 30)
-        df.at[indice,data] = date4
-        indice = indice + 1
-        flag = 60
-        topic_rows = topic_rows + 1
-    for flag in range(60,int(validade)):
-        flag = flag + 30
-        if flag % 30 == 0:
-            date4 = date4 + timedelta(days =30)
-            df.at[indice,data] = date4
-            indice = indice + 1
-            topic_rows = topic_rows + 1        
-    df1 = df
-    coluna = coluna + 1
-    return df1,topic_rows
+def calcula_dias_dif(coluna,date1,validade):#mudar o número do atributo "Dias" com o acréscimo da posição da coluna
+  while True:
+    try:
+      date1 = date.fromisoformat(date1)
+      flag = 1
+      indice = 1
+      data = "Dia " + str(coluna)
+      df = pd.DataFrame({data:[date1]})
+      if flag == 1:
+          date2 = date1 + timedelta(days = 1)
+          df.at[indice,data] = date2
+          flag = 7
+          topic_rows = 1 
+          indice = indice + 1
+      if flag == 7: 
+          date3 = date1 + timedelta(days = 7)
+          df.at[indice,data] = date3
+          flag = 30
+          indice = indice + 1
+          topic_rows = topic_rows + 1
+      if flag == 30:
+          date4 = date1 + timedelta(days = 30)
+          df.at[indice,data] = date4
+          indice = indice + 1
+          flag = 60
+          topic_rows = topic_rows + 1
+      for flag in range(60,int(validade)):
+          flag = flag + 30
+          if flag % 30 == 0:
+              date4 = date4 + timedelta(days =30)
+              df.at[indice,data] = date4
+              indice = indice + 1
+              topic_rows = topic_rows + 1 
+      df1 = df
+      coluna = coluna + 1
+      return df1,topic_rows 
+      break
+    except (ValueError,TypeError):
+      print("Data apenas aceita no formato 'YYYY-MM-DD'!\n")
+      date1 = input("Insira a data de início de contagem dessa forma '2020-12-30':")#função de data
+      validade = int(input("Prazo de validade (em dias):"))#função de data
 
 def the_seeker(arquivo):
   continuar = True
@@ -83,26 +91,30 @@ def the_seeker(arquivo):
   df = pd.read_csv(arquivo)# procurar arquivo no formato .csv
   print(df)
   while continuar == True:
-    seek_data = input("Procurar por data(ex.: 2020-11-19): ")
-    #instruções para procurar na tabela
-    i = 0
-    rows, col = df.shape
-    limite = int(col/2)
-    for i in range(0,rows):
-      n=1
-      if n == 1:
-        if df.at[i,'Dia'] == seek_data:
-          col_id = df.columns.get_loc('Dia')
-          print("\n"+df.columns[col_id+1],"-", df.at[i,'Dia'],"-", df.iat[i,col_id+1])          
-        n = 2
-      for n in range (2,limite+1,1):
-        string = "Dia "+str(n)
-        if df.at[i,string] == seek_data:
-          col_id = df.columns.get_loc(string)
-          print(df.columns[col_id+1],"-", df.at[i,string],"-", df.iat[i,col_id+1])
-    choice = input("1) Pesquisar mais alguma data no mesmo arquivo.             2) Tela Inicial.\n")
-    if choice == "2":
-      continuar = False
+    try:
+      seek_data = input("Procurar por data(ex.: 2020-11-19): ")
+      #instruções para procurar na tabela
+      i = 0
+      rows, col = df.shape
+      limite = int(col/2)
+      for i in range(0,rows):
+        n=1
+        if n == 1:
+          if df.at[i,'Dia'] == seek_data:
+            col_id = df.columns.get_loc('Dia')
+            print("\n"+df.columns[col_id+1],"-", df.at[i,'Dia'],"-", df.iat[i,col_id+1])          
+          n = 2
+        for n in range (2,limite+1,1):
+          string = "Dia "+str(n)
+          if df.at[i,string] == seek_data:
+            col_id = df.columns.get_loc(string)
+            print(df.columns[col_id+1],"-", df.at[i,string],"-", df.iat[i,col_id+1])
+      choice = input("1) Pesquisar mais alguma data no mesmo arquivo.             2) Tela Inicial.\n")
+      if choice == "2":
+        return 5 #acabar com o while loop do menu principal, se não, ao encerrar essa funão, ele pede para inserir o nome do arquivo novamente
+        continuar = False
+    except ((ValueError,TypeError,KeyError)):
+      print("Parece que essa data não existe na Tabela. Tente outra vez.")
 
 def create_column_base(assunto,time_min,start_day,end_day):
     # Chamar função para criar as datas para a tabela:
@@ -150,9 +162,19 @@ def new_file():#solicita info essenciais para criar uma coluna na tabela
   while continuar == True:
     # Info fornecida pelo usuário
     assunto = input("Assunto %d:" % flag)
-    time_min = float(input("Tempo de aprendizagem (em minutos):"))#em minutos
-    start_day = input("Insira a data de início de contagem dessa forma '2020-12-30':")#função de data
-    end_day = int(input("Prazo de validade (em dias):"))#função de data
+    while True:
+      try:
+        time_min = float(input("Tempo de aprendizagem (em minutos):"))#em minutos]
+        break
+      except (ValueError,TypeError):
+        print("Digite um valor numérico! E use '.' ao invés de ',' para decimais.")
+    while True:
+      try:
+        start_day = input("Insira a data de início de contagem dessa forma '2020-12-30':")#função de data
+        end_day = int(input("Prazo de validade (em dias):"))#função de data
+        break
+      except (ValueError,TypeError):
+        print("Data apenas aceita no formato 'YYYY-MM-DD'.\nO prazo da contagem é em números inteiros de dias.")
     # Primeiro assunto cria a tabela (os posteriores são emendados à primeira)
     if flag == 1:
       retorno = create_column_base(assunto,time_min,start_day,end_day)
@@ -167,7 +189,7 @@ def new_file():#solicita info essenciais para criar uma coluna na tabela
         flag = flag + 1
         break
       elif mais == "N" or mais == "n":
-        arquivo = input("Coloque um nome no seu arquivo(.csv):\n")
+        arquivo = input("Coloque um nome no seu arquivo(.csv, mas não precisa indicar a extensão).\nNada de colocar sinais como em \"][´~'\", porque pode até passar aqui, mas não será encontrado na pesquisa.\n")
         retorno.to_csv(arquivo, index=False)
         print("Seu arquivo está pronto. Olhe na pasta do programa!")
         print("Para pesquisar nele,vá na opção 2 e insira '%s' e uma data que você gostaria de saber se existem revisões." % arquivo)        
@@ -191,19 +213,19 @@ def main():
           break
         arquivo = input("Insira o nome do arquivo para a consulta: ")
         count = 1
-        while count in range (1,5):
+        while True:
           try:
-            the_seeker(arquivo)
+            count = the_seeker(arquivo)
             break
-          except FileNotFoundError:
+          except (FileNotFoundError,pd.errors.EmptyDataError):
             if count == 4 or count == 5:
               break
             arquivo = input("Arquivo não encontrado. Talvez seja erro de digitação. Tente novamente:\n")
             count = count + 1
             if count == 3:
               print("Arquivo não encontrado. Verifique se você digitou corretamente, se o arquivo está fora da pasta do programa,\nse é um arquivo CSV, ou se a existência dele não é só fruto da sua imaginação.")
-              choice = input("Deseja cancelar a pesquisa? S/N\n")
               while True:
+                choice = input("Deseja cancelar a pesquisa? S/N\n")
                 if choice == "S" or choice == "s":
                   count = 5
                   break
